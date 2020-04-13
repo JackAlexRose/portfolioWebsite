b356059d5eefe2a55b825f2a6d9de09f061d0136
JackAlexRose <jackalexanderrose@hotmail.com>
2020-04-13 14:41:14 +0100

    Added fixed navbar

--- js/animatedscroll.js
+++ js/animatedscroll.js
@@ -4,7 +4,7 @@ $(function() {
       if (target.length) {
         $("html, body").animate(
           {
-            scrollTop: target.offset().top
+            scrollTop: target.offset().top - $("#nav").height() - 20
           },
           1000
         );
